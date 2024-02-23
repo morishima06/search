66f8ea693856c8f297be285736a253bdbf30c2ba
森島瞬也 <245224aa@gmail.com>
2024-02-24 01:11:45 +0900

    change:余白修正

--- resources/views/home/checkout/guestcheckout.blade.php
+++ resources/views/home/checkout/guestcheckout.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.checkoutFrame')
 @section('content')
-    <div class="flex justify-center">
-       <p class="tetx-lg font-extrabold mt-20">ログインしてください</p>
-    </div>
-@endsectiongit
+<div class="flex justify-center">
+    <p class="tetx-lg font-extrabold mt-20">ログインしてください</p>
+</div>
+@endsection
